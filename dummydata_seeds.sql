90f2a1db9a46726bd6bfc332ad5b9bbfa9b2d473
Romi Keerbs <rskeerbs@gmail.com>
2020-05-26 14:26:27 -0700

    dummy data for testing

--- dummydata_seeds.sql
+++ dummydata_seeds.sql
@@ -5,3 +5,18 @@ VALUES ("John", "Smith", "jsmith");
 
 INSERT INTO user_id (first_name, last_name, user_id)
 VALUES ("Jane", "Doe", "jdoe");
+
+INSERT INTO user_meds (user_id, med_name, dosage, no_of_tablets, frequency)
+VALUES ("jsmith", "Actos", "15 mg", 1, 1);
+
+INSERT INTO user_meds (user_id, med_name, dosage, no_of_tablets, frequency)
+VALUES ("jsmith", "Glipizide", "10 mg", 1, 1);
+
+INSERT INTO carb_entry (user_id, carb_amount)
+VALUES ("jsmith", 30);
+
+INSERT INTO bgl_entry (user_id, glucose)
+VALUES ("jsmith", 116);
+
+INSERT INTO bp_entry (user_id, systolic, diastolic)
+VALUES ("jsmith", 130, 50);
