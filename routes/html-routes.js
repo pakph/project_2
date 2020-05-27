9a170641c9036baa75c443494d40aafe27419938
Romi Keerbs <rskeerbs@gmail.com>
2020-05-27 12:14:59 -0700

    changed model and handlebars

---
+++ routes/html-routes.js
@@ -0,0 +1,41 @@
+// Dependencies
+// =============================================================
+var path = require("path");
+
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+    app.get("/api/meds", function(req, res) {
+        db.Med.findAll({}).then(function(data) {
+            res.json(data);
+            console.log(data);
+        });     
+    });
+
+    // Each of the below routes just handles the HTML page that the user gets sent to.
+
+    // route loads Trackr.handlebars
+    app.get("/Trackr", function(req, res) {
+        res.render("Trackr", {});
+    });
+
+    // route loads Carb-Trackr.handlebars
+    app.get("/Carb-Trackr", function(req, res) {
+        res.render("Carb-Trackr", {});
+    });
+
+    // route loads Medication.handlebars
+    app.get("/Medications", function(req, res) {
+        res.render("Medication", {});
+    });
+
+
+    // route loads Homepage.html
+    app.get("/", function(req, res) {
+        console.log(__dirname);
+        res.sendFile(path.join(__dirname, "../public/Homepage.html"));
+    });
+};
---
+++ views/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $(document).on("submit", "#trackr-form", function(event){
+        event.preventDefault();
+        console.log("hello")
+    });
+    // need to pull up the modal when clicking on it
+    $('.modal').modal();
+
+    // for dropdown button in medications page
+    $('.dropdown-trigger').dropdown();
+
+    // function for the search input on the carb-trackr page
+
+    // api key
+    var key = "https://platform.fatsecret.com/js?key=d8db07bcdf444a4e829b355e25117d57&auto_nav=false"
+
+    function doLoad() {
+        fatsecret.setContainer("my_container");
+        fatsecret.setCanvas("foods.search");
+    };
+   
+});
+
+
+    
+
+
